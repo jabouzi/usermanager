5f740999080947e5c0be6de23efd81a09236d3b7
Skander Jabouzi <jabouzi@gmail.com>
2014-12-24 03:44:40 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/usermanager/trunk@9 98a39789-0e8e-4a56-b462-bd847eb66538

--- app/helper/functions.php
+++ app/helper/functions.php
@@ -1,47 +1,12 @@
 <?php
 
-function get_projects($user_projects = array())
-{
-	$sClient = new SoapClient('http://svn.tgiprojects.com/wsdl/usvnws.wsdl', array('trace' => 1));
-	$projects = $sClient->getlist();
-	$selected = '';
-	if (in_array('*', $user_projects)) $selected = 'selected';
-	echo '<select id="user_vhost" name="user_vhost[]" multiple="multiple" data-validate="required" >
-				<option value="*" '.$selected.'>All</option>';
-	foreach($projects as $project)
-	{
-		$selected = '';
-		if (in_array($project['projects_sitestaging'], $user_projects)) $selected = 'selected';
-		echo '<option value="'.$project['projects_sitestaging'].'" '.$selected.'>'.$project['projects_sitestaging'].'</option>';
-	}
-	echo '</select>
-	<span id="projects">
-	</span>';
-}
-
-function adjust_vhosts($vhosts)
-{
-	$sClient = new SoapClient('http://svn.tgiprojects.com/wsdl/usvnws.wsdl', array('trace' => 1));
-	$projects = $sClient->getlist();
-	if (in_array('*', $vhosts)) return array('*');
-	else if (!in_array('*', $vhosts) && count($vhosts) == count($projects)) return array('*');
-	else return $vhosts;
-}
-
 function compare_user_data($user1, $user2)
 {
 	if (isempty($user1['user_password'])) $user2['user_password'] = '';
 	$a1 = $user1;
-	$a2 = $user1['user_vhost'];
-	sort($a2);
-	unset($a1['user_vhost']);
-
 	$b1 = $user2;
-	$b2 = $user2['user_vhost'];
-	sort($b2);
-	unset($b1['user_vhost']);
 
-	return array_merge(array_diff_assoc($a1, $b1),array_diff_assoc($a2, $b2),array_diff_assoc($b1, $a1),array_diff_assoc($b2, $a2));
+	return array_merge(array_diff_assoc($a1, $b1), array_diff_assoc($b1, $a1));
 }
 
 function compare_user_admin($user1, $user2)
