85b45247bebf1585db80f810839567c6e05ce5ef
Skander Jabouzi <jabouzi@gmail.com>
2015-09-21 10:10:21 +0000

    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/usermanager/trunk@95 98a39789-0e8e-4a56-b462-bd847eb66538

--- newtemplate/includes/inc_left-menu.php
+++
@@ -1,9 +0,0 @@
-<div id="left-container">
-	<ul class="left-menu">
-		<li><a title="Gestion des administrateurs" href="index.php?source=administrateurs">Administrateurs</a></li>
-		<li><a title="Gestion des athlètes" href="index.php?source=athletes">Athlètes</a></li>
-		<li><a title="Gestion des bénévoles" href="index.php?source=benevols">Bénévoles</a></li>
-		<li><a title="Gestion des officiels" href="index.php?source=officiels">Officiels</a></li>
-		<li><a title="Gestion des clubs" href="index.php?source=clubs">Clubs</a></li>
-	</ul>
-</div>
