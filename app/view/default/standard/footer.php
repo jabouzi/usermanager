1c64e7128bfa721aaefc5f4bb3a5e13b4b224ce5
jabouzi <jabouzi@gmail.com>
2017-02-19 05:29:37 -0600

    remove warning text

--- app/view/default/standard/footer.php
+++ app/view/default/standard/footer.php
@@ -5,7 +5,12 @@
 						<?php display_languages(); ?>
 					</div>
 				</div>
+				<span style="color:red"><?php //echo lang('demo.warning'); ?></span>
 			</div>
 		</div>
+<a title="Real Time Web Analytics" href="http://clicky.com/100772563"><img alt="Real Time Web Analytics" src="//static.getclicky.com/media/links/badge.gif" border="0" /></a>
+<script src="//static.getclicky.com/js" type="text/javascript"></script>
+<script type="text/javascript">try{ clicky.init(100772563); }catch(e){}</script>
+<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100772563ns.gif" /></p></noscript>
 	</body>
 </html>
