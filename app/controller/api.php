00c1a86eead7c8b63b8eb8c2ed68f0797468d68f
Skander Jabouzi <jabouzi@gmail.com>
2017-02-25 16:02:30 -0500

    update api get profile

--- app/controller/api.php
+++ app/controller/api.php
@@ -36,8 +36,9 @@ class Api extends Controller
 	{
 		$encrypt = new encryption();
 		$user = $this->usermodel->get_user($email);
-		$user->set_password($encrypt->decrypt($user->get_password()));
-		echo json_encode($user->__toArray());
+		$result = $user->__toArray();
+		$result['password'] = $encrypt->decrypt($user->get_password());
+		echo json_encode($result);
 	}
 
 	public function delete($username)
